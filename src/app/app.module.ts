0a945a59bb4772e5ac473265fd13252204c867ff
DPostnik <DPostnik>
2021-03-12 12:56:50 +0300

    add routing and standart styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PostPageComponent } from './components/post-page/post-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomePageComponent,
+    PostPageComponent
   ],
   imports: [
     BrowserModule,
